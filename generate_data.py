838c82253430e2eab3c5692158d99a6ff370ff00
hengyuf <1369188290@qq.com>
2024-05-23 21:54:34 +0800

    genshin impact

--- generate_data.py
+++ generate_data.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-class PARAMS:
+class Data_generator:
     def __init__(self, _alpha_r=0, _beta_r=1, _d=1, _alpha_u=0, _beta_u=1, _gamma=1, _theta=0, __lambda=1):
         self.alpha_r = _alpha_r
         self.beta_r = _beta_r
@@ -21,5 +21,5 @@ class PARAMS:
             r[i]=self.alpha_r+self.beta_r*u+eps
         return r
     
-params = PARAMS(0.2, 0.2, 6.0, 0.6, 0.4, 0.1, 0.02, 2.5)
+params = Data_generator(0.2, 0.2, 6.0, 0.6, 0.4, 0.1, 0.02, 2.5)
 print(params.gen_data(5, 5))    
