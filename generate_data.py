e3fa6994f7aa60ff08c8b541d55a292194981f8a
hengyuf <1369188290@qq.com>
2024-05-23 21:57:09 +0800

    genshin impact

--- generate_data.py
+++ generate_data.py
@@ -21,5 +21,21 @@ class Data_generator:
             r[i]=self.alpha_r+self.beta_r*u+eps
         return r
     
-params = Data_generator(0.2, 0.2, 6.0, 0.6, 0.4, 0.1, 0.02, 2.5)
-print(params.gen_data(5, 5))    
+    def gen_data_full(self, n=1000, T=100):
+        r=np.zeros((T,n)) #r yield
+        eps_list=np.zeros((T,n))
+        u_list=np.zeros((T,n))
+        u=np.zeros(n)
+        eps=np.zeros(n)
+        for i in range(T):
+            u = self.alpha_u+self.beta_u*u+self.gamma*eps**2+self.theta*np.where(eps<0,eps**2,0)+np.random.exponential(scale=1/self._lambda,size=n)
+            eps=np.random.standard_t(df=self.d, size=n)*np.sqrt(u)
+            u_list[i]=u
+            eps_list[i]=eps
+            r[i]=self.alpha_r+self.beta_r*u+eps
+        return r, u_list, eps_list
+    
+    
+    
+#DG = Data_generator(0.2, 0.2, 6.0, 0.6, 0.4, 0.1, 0.02, 2.5)
+#print(DG.gen_data_full(5, 5))    
