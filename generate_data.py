03d2e44d579c981f711be8f8c82b78bec5dd1919
Chenruitao21 <138296011+Chenruitao21@users.noreply.github.com>
2024-05-23 21:50:26 +0800

    generate_data.py

--- generate_data.py
+++ generate_data.py
@@ -11,15 +11,15 @@ class PARAMS:
         self.theta = _theta
         self._lambda = __lambda
 
-def gen_data(params, n=1000, T=100):
-    r=np.zeros((T,n)) #r yield
-    eps=np.zeros(n)
-    u=np.zeros(n)
-    for i in range(T):
-        u = params.alpha_u+params.beta_u*u+params.gamma*eps**2+params.theta*np.where(eps<0,eps**2,0)+np.random.exponential(scale=1/params._lambda,size=n)
-        eps=np.random.standard_t(df=params.d, size=n)*np.sqrt(u)
-        r[i]=params.alpha_r+params.beta_r*u+eps
-    return r
+    def gen_data(self, n=1000, T=100):
+        r=np.zeros((T,n)) #r yield
+        eps=np.zeros(n)
+        u=np.zeros(n)
+        for i in range(T):
+            u = self.alpha_u+self.beta_u*u+self.gamma*eps**2+self.theta*np.where(eps<0,eps**2,0)+np.random.exponential(scale=1/self._lambda,size=n)
+            eps=np.random.standard_t(df=self.d, size=n)*np.sqrt(u)
+            r[i]=self.alpha_r+self.beta_r*u+eps
+        return r
     
 params = PARAMS(0.2, 0.2, 6.0, 0.6, 0.4, 0.1, 0.02, 2.5)
-gen_data(params, 5, 5)
+print(params.gen_data(5, 5))    
