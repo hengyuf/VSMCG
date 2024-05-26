235bc1ac7b33941c8f1db3c4c3fa39ba6444069f
Aureliussrc <160207565+Aureliussrc@users.noreply.github.com>
2024-05-26 11:23:48 +0800

    calculate p(r_t,eps_t|r_t1,eps_t1)

---
+++ updater.py
@@ -0,0 +1,11 @@
+import numpy as np
+from scipy.stats import t, expon
+
+
+def updaterr(r_t, eps_t, r_t1, eps_t1, alpha_r, beta_r, d, alpha_u, beta_u, gamma, theta, _lambda):
+    u_t1 = (r_t1 - alpha_r - eps_t1) / beta_r
+    w_t = alpha_u + beta_u * u_t1 + gamma * eps_t1 ** 2 + theta * (eps_t1 < 0) * eps_t1 ** 2
+    nu_t = eps_t * np.sqrt(beta_r / (r_t - eps_t - alpha_r))
+    eta_t = (r_t - eps_t - alpha_r) / beta_r - w_t
+    p = t.pdf(nu_t, d) * expon.pdf(eta_t, scale=1 / _lambda) / np.sqrt(beta_r * (r_t - eps_t - alpha_r))
+    return p
